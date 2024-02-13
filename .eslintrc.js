b6dd1c4dfe3d9bd2079e7302bcb87446b934e85d
SE <se@develup.pro>
2024-02-13 02:19:11 +0200

    refactor: update packages

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  root: true,
+  env: {
+    'browser': true,
+    'es2021': true,
+    'node': true,
+    'commonjs': true,
+    'react-native/react-native': true,
+  },
+  plugins: [
+    'react',
+    'react-native',
+    'react-hooks',
+    '@typescript-eslint',
+    'prettier',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        quoteProps: 'consistent',
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'es5',
+        useTabs: false,
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
+};
