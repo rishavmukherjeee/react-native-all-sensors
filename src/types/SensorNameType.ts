de501ae0f4ef8ee4406151521bc9ba4835db102b
SE <se@develup.pro>
2024-02-13 02:22:27 +0200

    refactor: update types

---
+++ src/types/SensorNameType.ts
@@ -0,0 +1,14 @@
+export type SensorNameType =
+  | 'proximity'
+  | 'light'
+  | 'accelerometer'
+  | 'magneticField'
+  | 'gyroscope'
+  | 'gravity'
+  | 'linearAcceleration'
+  | 'rotationVector'
+  | 'ambientTemperature'
+  | 'relativeHumidity'
+  | 'pressure'
+  | 'temperature'
+  | 'orientation';
